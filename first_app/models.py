32fc785d98a6b376223d53709985254e7bb07531
nahid <nahidkishore99@gmail.com>
2020-07-26 18:52:09 +0600

    model objects

--- first_app/models.py
+++ first_app/models.py
@@ -7,6 +7,10 @@ class Musician(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     instrument = models.CharField(max_length=100)
+    
+    
+    def __str__(self):
+        return self.first_name + " " + self.last_name
 
 class Album(models.Model):
 	# id = models.AutoField(primary_key=True)
