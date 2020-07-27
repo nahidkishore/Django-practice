816192c885c3c2c666109ccaf817c9c9b9f87ff6
nahid <nahidkishore99@gmail.com>
2020-07-27 23:30:51 +0600

    CREATING A DJANGO FORM 3

--- first_app/forms.py
+++ first_app/forms.py
@@ -4,6 +4,11 @@ from django import forms
 
 class user_form(forms.Form):
   user_form = forms.CharField( label="Full Name", initial="nahid")
-  user_email = forms.EmailField(label= "User Email")
+  user_email = forms.EmailField(label= "User Email",  widget =forms.TextInput(attrs= {'placeholder': 'Enter Your Email', 'style': 'width:300px'}))
+  
+  user_dob=forms.DateField(label= "Date of Birth", widget=forms.TextInput(attrs={'type': 'date'}))
+  
+  user_dob1=forms.DateField(label= "Date of Birth", widget=forms.DateInput(attrs={'type': 'date'}))
+  
   
 
