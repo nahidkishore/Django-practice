cea8bac5516a679bbbf538d509e57f3efbdcde22
nahid <nahidkishore99@gmail.com>
2020-07-27 22:41:49 +0600

    CREATING A DJANGO FORM 2

--- first_app/forms.py
+++ first_app/forms.py
@@ -3,7 +3,7 @@ from django import forms
 
 
 class user_form(forms.Form):
-  user_form = forms.CharField()
-  user_email = forms.EmailField()
+  user_form = forms.CharField( label="Full Name", initial="nahid")
+  user_email = forms.EmailField(label= "User Email")
   
 
