50ae18b96dff587c48cffa784c5f4b1deefadc7d
nahid <nahidkishore99@gmail.com>
2020-08-10 12:42:00 +0600

    Entry Forms

--- first_app/forms.py
+++ first_app/forms.py
@@ -1,14 +1,25 @@
 from django import forms
-from django.core import validators
 from first_app import models
 
+class MusicianForm(forms.ModelForm):
+    class Meta:
+        model = models.Musician
+        fields = "__all__"
+        
+        
+        
+        
+class AlbumForm(forms.ModelForm):
+    release_date = forms.DateField(widget=forms.TextInput(attrs={'type':'date'}))
+    class Meta:
+        model = models.Album
+        fields = "__all__"
 
 
-class  MusicianForm (forms.ModelForm):
-  
-  class Meta:
-    model = models.Musician
-    fields = "__all__"
+
+
+
+
     
     # jodi specific kono kicho bad dite chai, tahole  exclude use korbo.....    exclude =['first_name]
     
--- first_app/views.py
+++ first_app/views.py
@@ -6,7 +6,8 @@ from first_app import forms
 # Create your views here.
 
 def index(request):
-  diction= {'title': "Home Page"}
+  musician_list= Musician.objects.order_by('first_name')
+  diction= {'title': "Home Page", 'musician_list': musician_list}
   return render(request, 'first_app/index.html', context=diction)
 
 
@@ -15,9 +16,30 @@ def album_list(request):
   return render (request, 'first_app/album_list.html', context = diction)
 
 def musician_form(request):
-  diction={'title': 'Add Musician'}
+  form=forms.MusicianForm()
+  
+  if request.method == 'POST':
+    form =forms.MusicianForm(request.POST)
+    
+    if form.is_valid():
+      form.save(commit=True)
+      return index(request)
+  
+  diction={'title': 'Add Musician', 'musician_form':form}
   return render(request, 'first_app/musician_form.html', context=diction)
 
 def album_form(request):
-  diction={'title': 'Add Album'}
+  form=forms.AlbumForm()
+  
+  if request.method == 'POST':
+    form =forms.AlbumForm(request.POST)
+    
+    if form.is_valid():
+      form.save(commit=True)
+      return index(request)
+      
+      
+  diction={'title': 'Add Album', 'album_form':form}
   return render (request, 'first_app/album_form.html', context=diction)
+
+
