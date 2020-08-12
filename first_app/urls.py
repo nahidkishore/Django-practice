92b153fc1b6c02f5b6dbc4576895876f64c8153e
nahid <nahidkishore99@gmail.com>
2020-08-13 00:11:16 +0600

    Update Data Part 3

--- first_app/urls.py
+++ first_app/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('add_musician/', views.musician_form, name='musician_form'),
     path('album_list/<int:artist_id>/', views.album_list, name='album_list'),
     path('edit_artist/<int:artist_id>/',views.edit_artist, name='edit_artist'),
+    path('edit_album/<int:album_id>/', views.edit_album, name='edit_album'),
   
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -62,11 +62,27 @@ def edit_artist(request,artist_id):
     if form.is_valid():
       form.save(commit=True)
       return album_list(request, artist_id )
-      
-    
-    
   
   diction={'edit_form':form}
   return render(request, 'first_app/edit_artist.html', context= diction)
+
+
+
+def edit_album(request, album_id):
+  
+  album_info=Album.objects.get(pk=album_id)
+  form=forms.AlbumForm(instance=album_info)
+  diction= {}
+  
+  if request.method=='POST':
+    form=forms.AlbumForm(request.POST, instance=album_info)
+    
+    
+    if form.is_valid():
+      form.save(commit=True)
+      diction.update({'success_text':'successfully updated!'})
+      
+  diction.update({'edit_form':form})
+  return render(request, 'first_app/edit_album.html', context= diction)
 
 
